f9067335fc287aa061c5a0ef8637e4800cb49f61
alderjunio <alder.junio@gmail.com>
2025-08-10 22:15:03 -0400

    criado card com dados mocados

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,6 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.css'
 })
-export class BigCardComponent {
+
+export class BigCardComponent implements OnInit{
+
+@Input()
+photoCover:string = "";
+
+@Input()
+cardTitle:string = "";
+
+@Input()
+cardDescription:string = "";
+
+
+
+constructor(){}
+  ngOnInit(): void {
+    
+  }
 
 }
